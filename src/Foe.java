352ea20755b5f98d36b17b2f27d2ba9781358ef6
mesuara <mesuara@gmail.com>
2024-05-20 14:48:04 -0400

    added assets for zombie fm

--- src/Foe.java
+++ src/Foe.java
@@ -1,9 +1,9 @@
 
 public class Foe extends Sprite{
-	public static String[] pose = {"Attack", "Idle", "Walk"};
+	public static String[] pose = {"Attack", "Idle", "Walk", "Death"};
 	
 	public Foe(int x, int y) {
-		super("Zombie-", pose, x, y, 5, 15);
+		super("Zombie_", pose, x, y, 5, 15);
 	}
 	
 	
