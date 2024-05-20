a851c579124121602cbce62ed2cfd09723fb3677
Jonathan M Cordero <48733516+JonathanCordero@users.noreply.github.com>
2024-05-20 17:21:20 -0400

    Update MeowKnight.java
    slashing animation added, also heads up I have the graphics and paint method because I was going to start working on the slash hitbox, but neither will be needed at the end.

--- src/MeowKnight.java
+++ src/MeowKnight.java
@@ -1,14 +1,26 @@
+import java.awt.Graphics;
 
 public class MeowKnight extends Sprite {
 
 	public static String[] pose = {"IdleLeft", "Idle", "RunLeft", "Run",
-					"Attack_1","Attack_1Left","DeathLeft", "Death",
-					"Take_DamageLeft", "Take_Damage", "Attack_2Left","Attack_2",
-					"DodgeLeft", "Dodge", "JumpLeft","Jump",
-					"ReflectLeft","Reflect"};
+									"AttackLeft_1","Attack_1","DeathLeft", "Death",
+									"Take_DamageLeft", "Take_Damage", "AttackLeft_2","Attack_2",
+									"DodgeLeft", "Dodge", "JumpLeft","Jump",
+									"ReflectLeft","Reflect"};
 	
 	public MeowKnight(int x, int y) {
 		super("Meow-Knight", pose, x, y, 5, 15);
 	}
 
+	public void attack() {
+		if (facingleft)action = 4;
+		else action = 5;
+		
+		moving = true;
+		Rect slash = new Rect (x, y, 80, 30);
+	}
+	
+	/*public void draw(Graphics g) {
+		super.draw(g);
+	}*/
 }
