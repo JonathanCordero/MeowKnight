8b437149da846ed369980a52ac9fac4acd7f7ae0
mesuara <mesuara@gmail.com>
2024-05-20 09:12:04 -0400

    fogot dash for image

--- src/Foe.java
+++ src/Foe.java
@@ -3,7 +3,7 @@ public class Foe extends Sprite{
 	public static String[] pose = {"Attack", "Idle", "Walk"};
 	
 	public Foe(int x, int y) {
-		super("Zombie", pose, x, y, 5, 15);
+		super("Zombie-", pose, x, y, 5, 15);
 	}
 	
 	
