021cb476c72cce2f08bced845008bcbb81500ba5
Jonathan M Cordero <48733516+JonathanCordero@users.noreply.github.com>
2024-05-20 17:22:50 -0400

    Update Foe.java
    Abstract added, same reason as provided before

--- src/Foe.java
+++ src/Foe.java
@@ -2,10 +2,16 @@
 public class Foe extends Sprite {
 
 	public static String[] pose = {"Idleleft", "Idle", "WalkLeft", "Walk",
-					"AttackLeft", "Attack", "DeadLeft", "Dead"};
+									"AttackLeft", "Attack", "DeadLeft", "Dead"};
 	
 	public Foe(int x, int y) {
 		super("Zombie", pose, x, y, 5, 15);
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
