11c7265e2487cc978fd7e21f0df46e1c60571920
Jonathan M Cordero <48733516+JonathanCordero@users.noreply.github.com>
2024-05-20 15:10:33 -0400

    Update MeowKnight.java
    Uniformed array and now has left facing added, but i might move the string pose into sprite and create a larger array stored there, however might not be worth it since some units could have flying instead of walking, i digress.

--- src/MeowKnight.java
+++ src/MeowKnight.java
@@ -1,10 +1,11 @@
 
 public class MeowKnight extends Sprite {
 
-	public static String[] pose = {"Attack_1", "Attack_2", "Death", 
-			"Dodge", "Idle", "IdleLeft", 
-			"Jump","Reflect","Run", 
-			"RunLeft","Take_Damage"};
+	public static String[] pose = {"IdleLeft", "Idle", "RunLeft", "Run",
+					"Attack_1","Attack_1Left","DeathLeft", "Death",
+					"Take_DamageLeft", "Take_Damage", "Attack_2Left","Attack_2",
+					"DodgeLeft", "Dodge", "JumpLeft","Jump",
+					"ReflectLeft","Reflect"};
 	
 	public MeowKnight(int x, int y) {
 		super("Meow-Knight", pose, x, y, 5, 15);
